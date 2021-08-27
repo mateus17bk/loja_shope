03628d3619321131a8b7ff995ffdcf3883eb4426
Mateus Guedes <mateus.guedes@squidtelecom.com.br>
2021-08-27 12:11:41 -0300

    Subir sem os certificados

--- poc/pix-auth.js
+++ poc/pix-auth.js
@@ -1,7 +1,8 @@
 require('dotenv').config({ path: '../.env.homologacao' })
+
 console.log(process.env.GN_ENV)
 
-const https = require('https')
+/*const https = require('https')
 const axios = require('axios')
 const fs = require('fs')
 
@@ -11,8 +12,8 @@ const apiStaging = 'https://api-pix.gerencianet.com.br'
 const baseUrl = process.env.GN_ENV === 'producao' ? apiProduction : apiStaging
 const getToken = async () => {
     console.log('getToken', process.env.GN_ENV)
-    const certificado = fs.readFileSync('../'+process.env.GN_CERTIFICADO)
     
+    const certificado = fs.readFileSync('../'+process.env.GN_CERTIFICADO)
     const credenciais = {
         client_id: process.env.GN_CLIENT_ID,
         client_secret: process.env.GN_CLIENT_SECRET,
@@ -31,14 +32,62 @@ const getToken = async () => {
         method: 'POST',
         url: baseUrl + '/oauth/token',
         headers: {
-            Authorization: 'Basic '+auth,
-            'Content-Type': 'application/json'
+            Authorization: 'Basic '+ auth,
+            'Content-Type': 'application/json',
         },
         httpAgent: agent,
         data: data
     }
 
     const result = await axios(config)
-    console.log(result.data)
+    return(result.data)
 
 }
+
+const run = async () => {
+    const token = await getToken()
+    const accessToken = token.accessToken
+
+    const certificado = fs.readFileSync('../'+process.env.GN_CERTIFICADO)
+    const data = JSON.stringify({
+        calendario: {
+            expiracao: 3600
+        },
+
+        devedor: {
+            cpf: '12345678909',
+            nome: 'Mateus',
+        },
+
+        valor: {
+            original: '130.50',
+        }, 
+
+        chave:'aaaa',// pelo app
+        solicitacaoPagador: 'Cobrança dos serviços prestados'
+
+      })
+ 
+    const agent = new https.Agent({
+        pfx: certificado,
+        passphrase:'',
+    })
+
+    const config = {
+        method: 'POST',
+        url: baseUrl + '/v2/cob',
+        headers: {
+            Authorization: 'Bearer '+ accessToken,
+            'Content-Type': 'application/json',
+        },
+        httpAgent: agent,
+        data: data
+    }
+
+    const result = await axios(config)
+
+    console.log(result)
+
+}
+
+run()*/
